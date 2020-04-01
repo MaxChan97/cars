39f878edd6271279521ed82b89db5d7f3592ac10
MaxChan97 <maxchanwf@gmail.com>
2020-04-01 13:22:30 +0800

    first commit

---
+++ cars-ejb/src/java/ejb/session/stateless/AppointmentEntitySessionBean.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejb.session.stateless;
+
+import javax.ejb.Local;
+import javax.ejb.Remote;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author Max
+ */
+@Stateless
+@Local(AppointmentEntitySessionBeanLocal.class)
+@Remote(AppointmentEntitySessionBeanRemote.class)
+public class AppointmentEntitySessionBean implements AppointmentEntitySessionBeanRemote, AppointmentEntitySessionBeanLocal {
+
+    @PersistenceContext(unitName = "cars-ejbPU")
+    private EntityManager em;
+
+    
+
+    
+}
